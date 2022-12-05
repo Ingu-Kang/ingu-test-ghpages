3be7190b7218c732e726bbb899567e4464b6448f
Ingu Kang <conchuri1@gmail.com>
2022-12-06 02:07:19 +0900

    Eddited publicPath

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -82,7 +82,7 @@ module.exports = {
                 test: /\.(glb|gltf)$/,
                 loader: 'file-loader',
                 options: {
-                    publicPath: './dist',
+                    publicPath: './dist/models',
                     name: '[name].[ext]'
                }
             },
